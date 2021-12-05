feea5bd926e8f80235e6d81b6cb59fb345d2988c
Jonatan Borkowski <jonatan.borkowski@pm.me>
2021-12-05 18:17:51 +0100

    Bump GHC to 9.2.1

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        haskellPackages = pkgs.haskell.packages.ghc901;
+        haskellPackages = pkgs.haskell.packages.ghc921;
 
         packageName = "advent-of-code";
       in {
